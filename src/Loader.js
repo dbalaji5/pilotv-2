843821f5e9d5c23dc84575ec11cd534fc4dc02df
balajid <balajid@dal.ca>
2020-01-06 19:23:18 -0400

    ui loader updated

--- src/Loader.js
+++ src/Loader.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
 class Loader extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            data:[]
+        }
+    }
 
+    componentDidUpdate(oldProps,oldState){
+        if(this.props.data !== oldState.data){
+            this.setState({
+                data:this.props.data
+            });
+        }
+    }
     render(){
+        if(this.state.data.length==0){
+            return (
+                <h1>Waiting for the contents</h1>
+            );
+        }
         return(
-            <h1>Waiting for the Contents</h1>
-        )
+            <div> 
+            {this.state.data.map(item => 
+                <h1 key={item}>{item}</h1>    
+                
+            )}
+            </div>
+        );
     }
 }
 
