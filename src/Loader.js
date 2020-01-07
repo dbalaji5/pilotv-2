72a4596073fe22b5a6d22faa944ae79a5530c8ca
balajid <balajid@dal.ca>
2020-01-06 22:33:32 -0400

    button updated

--- src/Loader.js
+++ src/Loader.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import {Icon,Stack,Button} from '@shopify/polaris';
+import {ArrowUpMinor,ArrowDownMinor} from '@shopify/polaris-icons';
 
 class Loader extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            data:[]
+            data:[],
+            source:[]
         }
     }
 
+    changeSource=(i)=>{
+
+        var source=this.state.source;
+        if(source[i] === ArrowUpMinor){
+            source[i]=ArrowDownMinor
+        }
+        else{
+            source[i]=ArrowUpMinor
+        }
+        this.setState({
+            source:source
+        });
+    }
+
     componentDidUpdate(oldProps,oldState){
+    
         if(this.props.data !== oldState.data){
+            var source=this.state.source;
+            source.push(ArrowUpMinor)
+            if(this.props.data.length==0){
+                this.setState({
+                    source:[]
+                })
+            }
             this.setState({
-                data:this.props.data
+                data:this.props.data,
+                source:source
             });
+
         }
     }
     render(){
@@ -23,8 +50,11 @@ class Loader extends React.Component{
         }
         return(
             <div> 
-            {this.state.data.map(item => 
-                <h1 key={item}>{item}</h1>    
+            {this.state.data.map((item,index) =>
+                <Stack>
+                <p key={item}>{item}</p>
+                <Button icon={this.state.source[index]} onClick={()=>this.changeSource(index)}/>
+                </Stack>
                 
             )}
             </div>
