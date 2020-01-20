ae3b26e56922b8d468af1ceab6dbea103737b3cf
balajid <balajid@dal.ca>
2020-01-19 20:22:54 -0400

    frame

--- src/index.js
+++ src/index.js
@@ -1,34 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Comparator from './Comparator.js'
-import DashBoard from './DashBoard.js'
+// import DashBoard from './DashBoard.js'
+// import Comparator from './Comparator.js'
+import FrameBoard from './FrameBoard.js'
 ReactDOM.render(
-  <DashBoard/>,
+  <FrameBoard/>,
   document.getElementById('root')
 );
-// const Plot = createPlotlyComponent(Plotly);
 
-// ReactDOM.render(
-//   React.createElement(Plot, {
-//     data: [
-//       {
-//         type: 'scatter',
-//         mode: 'lines+points',
-//         x: [1, 2, 3],
-//         y: [2, 6, 3],
-//         marker: {color: 'red'}
-//       },
-//       {
-//         type: 'bar',
-//         x: [1, 2, 3],
-//         y: [2, 5, 3]
-//       }
-//     ],
-//     layout: {
-//       width: 640,
-//       height: 480,
-//       title: 'A Fancy Plot'
-//     }
-//   }),
-//   document.getElementById('root')
-// );
