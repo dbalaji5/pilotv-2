785221b650ba42e2c424577c2a2034f4312c1bbc
balajid <balajid@dal.ca>
2020-01-22 11:42:50 -0400

    ui layout fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import DashBoard from './DashBoard.js'
-// import Comparator from './Comparator.js'
 import FrameBoard from './FrameBoard.js'
 ReactDOM.render(
   <FrameBoard/>,
